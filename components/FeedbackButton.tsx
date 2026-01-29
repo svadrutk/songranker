c29b2965df7f097adefcb279436f944a0f1b4356
Swad Kukunooru <kukunoorusvadrut@gmail.com>
2026-01-29 01:23:04 -0500

    style: further increase feedback button bottom offset on mobile

--- components/FeedbackButton.tsx
+++ components/FeedbackButton.tsx
@@ -9,7 +9,7 @@ export function FeedbackButton() {
 
   return (
     <>
-      <div className="fixed bottom-6 md:bottom-4 left-4 z-[70]">
+      <div className="fixed bottom-12 md:bottom-4 left-4 z-[70]">
         <button
           onClick={() => setIsOpen(true)}
           className="h-10 w-10 rounded-full bg-yellow-500 flex items-center justify-center shadow-lg hover:bg-yellow-400 transition-colors"
